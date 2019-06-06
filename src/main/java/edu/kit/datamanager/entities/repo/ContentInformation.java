3fbfbf9dbf4262ade329a656b6f608ae8117a308
Thomas Jejkal <thomas.jejkal@kit.edu>
2019-06-06 13:11:24 +0200

    Added content information base entities

---
+++ src/main/java/edu/kit/datamanager/entities/repo/ContentInformation.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2017 Karlsruhe Institute of Technology.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.kit.datamanager.entities.repo;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.http.InvalidMediaTypeException;
+import org.springframework.http.MediaType;
+
+/**
+ *
+ * @author jejkal
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class ContentInformation implements Serializable{
+
+  private String relativePath;
+  private String contentUri;
+  private String mediaType;
+  private String hash;
+  private long size;
+  private Map<String, String> metadata = new HashMap<>();
+  private Set<String> tags = new HashSet<>();
+
+  public String getFilename(){
+
+    if(relativePath == null){
+      return null;
+    }
+    return relativePath.substring(relativePath.lastIndexOf("/") + 1);
+  }
+
+  @JsonIgnore
+  public void setMediaTypeAsObject(MediaType mediaType){
+    if(mediaType == null){
+      throw new IllegalArgumentException("Argument must not be null.");
+    }
+    this.mediaType = mediaType.toString();
+  }
+
+  @JsonIgnore
+  public MediaType getMediaTypeAsObject(){
+    try{
+      return MediaType.parseMediaType(mediaType);
+    } catch(InvalidMediaTypeException ex){
+      return MediaType.APPLICATION_OCTET_STREAM;
+    }
+  }
+}
